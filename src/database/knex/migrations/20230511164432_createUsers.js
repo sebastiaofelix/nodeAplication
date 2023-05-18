ca89c801e708b9340b80e0a15791ef1d243e9da0
sebastiao <sebastiaofdmn@gmail.com>
2023-05-18 18:38:27 -0300

    user

--- src/database/knex/migrations/20230511164432_createUsers.js
+++ src/database/knex/migrations/20230511164432_createUsers.js
@@ -2,7 +2,8 @@ exports.up = knex => knex.schema.createTable("user", table => {
     table.increments("id");
     table.text("name");
     table.text("email");
-    table.text("avatar");
+    table.text("password");
+    table.text("avatar").nullable();
     table.timestamp("created_at").default(knex.fn.now());
     table.timestamp("updated_at").default(knex.fn.now());
 });
